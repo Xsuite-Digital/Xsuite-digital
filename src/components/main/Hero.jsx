4a10ce6149f591b6eef101b18185cbf35e1afd07
Muhammad Anas <41787190+Anaskhaan@users.noreply.github.com>
2024-09-28 12:20:13 +0500

    video link & opacity updated

--- src/components/main/Hero.jsx
+++ src/components/main/Hero.jsx
@@ -2,19 +2,21 @@ import { motion } from "framer-motion";
 import { useTypewriter } from "react-simple-typewriter";
 
 const Hero = () => {
-
-  const [typeEffect] =useTypewriter({
-    words: [' Digital Marketer  ' , 'Web Developer  ' , 'UX/UI Designer ', 'Graphic Designer'],
+  const [typeEffect] = useTypewriter({
+    words: [
+      " Digital Marketer  ",
+      "Web Developer  ",
+      "UX/UI Designer ",
+      "Graphic Designer",
+    ],
     loop: {},
-    typeSpeed:100,
-    deleteSpeed:40,
-    
-
-  })
+    typeSpeed: 100,
+    deleteSpeed: 40,
+  });
   return (
     <div className="relative">
       <video
-        src="/blackvideo.mp4"
+        src="/blackvideoo.mp4"
         className="absolute inset-0 w-full h-full object-cover"
         loop
         autoPlay
@@ -24,7 +26,7 @@ const Hero = () => {
       </video>
 
       {/* Overlay with background opacity */}
-      <div className="absolute inset-0 bg-black opacity-90"></div>
+      <div className="absolute inset-0 bg-black opacity-50"></div>
 
       {/* Content Section */}
       <div className="flex flex-col justify-center items-center relative z-10 h-screen text-center">
@@ -34,19 +36,25 @@ const Hero = () => {
           transition={{ duration: 1 }}
           className="flex text-[#7E7E7E]"
         >
-          <img src="/Xsuite-Final.png" className="h-16 w-72 lg:w-full lg:h-36 " />
+          <img
+            src="/Xsuite-Final.png"
+            className="h-16 w-72 lg:w-full lg:h-36 "
+          />
         </motion.div>
 
-       
-<motion.div 
-initial={{ opacity: 0, y: -40 }}
-animate={{ y: 0, opacity: 1 }}
-transition={{ duration: 1 }}
-className=" flex items-center justify-center mt-4 space-x-4">
-  <motion.h1  className='font-bold text-xl lg:text-4xl  text-white  ' >We are</motion.h1>
-  <p className=" font-bold text-xl lg:text-4xl text-white">{typeEffect}</p>
-</motion.div>
-
+        <motion.div
+          initial={{ opacity: 0, y: -40 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 1 }}
+          className=" flex items-center justify-center mt-4 space-x-4"
+        >
+          <motion.h1 className="font-bold text-xl lg:text-4xl  text-white  ">
+            We are
+          </motion.h1>
+          <p className=" font-bold text-xl lg:text-4xl text-white">
+            {typeEffect}
+          </p>
+        </motion.div>
       </div>
     </div>
   );
