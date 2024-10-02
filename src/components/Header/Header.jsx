42098c32743c7c45b9b0b6ba5bda129d96d041c8
Mashhood-Rehman <mashhoodbutt47@gmail.com>
2024-10-02 15:07:15 +0500

    shukar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -39,7 +39,7 @@ const Header = () => {
           <Link to="https://www.facebook.com/profile.php?id=61566739650180">
             <img
               src="/fb11.png"
-              className="   h-6 w-6  p-1 hover:text-orange-500  text-white bg-transparent duration-300 ease-in-out hover:bg-transparent  "
+              className="   h-10 w-auto  p-1 hover:text-orange-500  text-white bg-transparent duration-300 ease-in-out hover:bg-transparent  "
               alt=""
             />
           </Link>
