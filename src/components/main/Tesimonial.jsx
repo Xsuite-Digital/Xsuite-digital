8afa2098e6d449476370fa006abd20d8c09917c3
Mashhood-Rehman <mashhoodbutt47@gmail.com>
2024-10-05 12:07:37 +0500

    okk

--- src/components/main/Tesimonial.jsx
+++ src/components/main/Tesimonial.jsx
@@ -1,223 +1,76 @@
 const Testimonial = () => {
+  const testimonials = [
+    {
+      name: "Holden Caulfield",
+      role: "UI DEVELOPER",
+      text: "Our brand needed a fresh take on digital marketing, and this agency delivered...",
+      imgSrc: "/p1.webp",
+      rating: 5,
+    },
+    {
+      name: "Alper Kamu",
+      role: "DESIGNER",
+      text: "I appreciate how transparent this marketing agency has been throughout our project...",
+      imgSrc: "/p2-min.webp",
+      rating: 4,
+    },
+    {
+      name: "Alper Kamu",
+      role: "DESIGNER",
+      text: "I appreciate how transparent this marketing agency has been throughout our project...",
+      imgSrc: "/p2-min.webp",
+      rating: 4,
+    },
+  
+  ];
+
   return (
     <section className="text-gray-600 body-font">
       <div className="container px-5 py-24 mx-auto">
-        <h1 className=" flex justify-center font-bold tracking-wider text-lg lg:text-3xl mb-4 text-black">
-          What client says?
+        <h1 className="flex justify-center font-bold tracking-wider text-lg lg:text-3xl mb-4 text-black">
+          What clients say?
         </h1>
         <div className="flex mt-6 justify-center">
-          <div className="w-16 h-1 rounded-full bg-black2 inline-flex" />
+          <div className="w-16 h-1 rounded-full bg-black inline-flex" />
         </div>
-        <div className="flex flex-wrap -m-4  rounded-md  mb-5">
-          {/* review */}
-          <div className="w-[350px] h-[27rem] p-4 mx-auto shadow-md rounded-xl relative group hover:scale-105 duration-300">
-            <div className="h-full bg-gray-100 p-8 rounded">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                className="block w-5 h-5 text--black2 mb-4"
-                viewBox="0 0 975.036 975.036"
-              >
-                <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z" />
-              </svg>
-              {/* rating */}
-              <div className="rating mb-4">
-                <input
-                  type="radio"
-                  name="rating-1"
-                  className="mask mask-star"
-                />
-                <input
-                  type="radio"
-                  name="rating-1"
-                  className="mask mask-star"
-                  defaultChecked
-                />
-                <input
-                  type="radio"
-                  name="rating-1"
-                  className="mask mask-star"
-                />
-                <input
-                  type="radio"
-                  name="rating-1"
-                  className="mask mask-star"
-                />
-                <input
-                  type="radio"
-                  name="rating-1"
-                  className="mask mask-star"
-                />
-              </div>
-
-              <p className="leading-relaxed mb-6 text-sm sm:text-base">
-                Our brand needed a fresh take on digital marketing, and this
-                agency delivered. Their creative team consistently came up with
-                unique ideas that resonated with our audience. From social media
-                campaigns to email marketing, their work has elevated our brand
-                presence.
-              </p>
-
-              <div className="flex items-center text-sm sm:text-base">
-                <img
-                  alt="testimonial"
-                  src="/p1.webp"
-                  className="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"
-                />
-                <span className="flex-grow flex flex-col pl-4">
-                  <span className="title-font font-medium text-gray-900">
-                    Holden Caulfield
+        <div className="flex flex-wrap -m-4 rounded-md mb-5">
+          {testimonials.map((testimonial, index) => (
+            <div key={index} className="w-full sm:w-[350px] h-[27rem] p-4 mx-auto shadow-md rounded-xl relative group hover:scale-105 duration-300">
+              <div className="h-full bg-gray-100 p-8 rounded">
+                {/* SVG and Rating */}
+                <div className="rating mb-4">
+                  {[...Array(5)].map((_, i) => (
+                    <label key={i}>
+                      <input
+                        type="radio"
+                        name={`rating-${index}`}
+                        className="mask mask-star"
+                        defaultChecked={i < testimonial.rating}
+                      />
+                      <span className="sr-only">{`${i + 1} star`}</span>
+                    </label>
+                  ))}
+                </div>
+                <p className="leading-relaxed mb-6 text-sm sm:text-base">{testimonial.text}</p>
+                <div className="inline-flex items-center">
+                  <img
+                    alt="testimonial"
+                    src={testimonial.imgSrc}
+                    className="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"
+                    loading="lazy"
+                  />
+                  <span className="flex-grow flex flex-col pl-4">
+                    <span className="title-font font-medium text-gray-900">{testimonial.name}</span>
+                    <span className="text-gray-600 text-sm">{testimonial.role}</span>
                   </span>
-                  <span className="text--gray text-sm">UI DEVELOPER</span>
-                </span>
+                </div>
               </div>
             </div>
-          </div>
-          {/* review2 */}
-          <div className="w-[350px] h-[27rem] p-4 mx-auto shadow-md rounded-xl relative group hover:scale-105 duration-300">
-            <div className="h-full bg-gray-100 p-8 rounded">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                className="block w-5 h-5 text--black2 mb-4"
-                viewBox="0 0 975.036 975.036"
-              >
-                <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z" />
-              </svg>
-              {/* rating */}
-              <div className="rating mb-4">
-                <label>
-                  <input
-                    type="radio"
-                    name="rating-1"
-                    className="mask mask-star"
-                  />
-                  <span className="sr-only">1 star</span>
-                </label>
-                <label>
-                  <input
-                    type="radio"
-                    name="rating-1"
-                    className="mask mask-star"
-                    defaultChecked
-                  />
-                  <span className="sr-only">2 stars</span>
-                </label>
-                <label>
-                  <input
-                    type="radio"
-                    name="rating-1"
-                    className="mask mask-star"
-                  />
-                  <span className="sr-only">3 stars</span>
-                </label>
-                <label>
-                  <input
-                    type="radio"
-                    name="rating-1"
-                    className="mask mask-star"
-                  />
-                  <span className="sr-only">4 stars</span>
-                </label>
-                <label>
-                  <input
-                    type="radio"
-                    name="rating-1"
-                    className="mask mask-star"
-                  />
-                  <span className="sr-only">5 stars</span>
-                </label>
-              </div>
-              <p className="leading-relaxed mb-6 text-sm sm:text-base">
-                I appreciate how transparent this marketing agency has been
-                throughout our project.From day one,they provided clear
-                timelines,and kept us informed at every stage. Monthly reports
-                were detailed and easy to understandhelping us track our return
-                on investment.
-              </p>
-
-              <div className="inline-flex items-center">
-                <img
-                  alt="testimonial"
-                  src="/p2-min.webp"
-                  className="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"
-                  loading = "lazy"
-                />
-                <span className="flex-grow flex flex-col pl-4">
-                  <span className="title-font font-medium text-gray-900">
-                    Alper Kamu
-                  </span>
-                  <span className="text--gray text-sm">DESIGNER</span>
-                </span>
-              </div>
-            </div>
-          </div>
-          {/* review3 */}
-          <div className="w-[350px] h-[27rem] p-4 mx-auto shadow-md rounded-xl relative group hover:scale-105 duration-300">
-            <div className="h-full bg-gray-100 p-8 rounded">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                className="block w-5 h-5 text--black2 mb-4"
-                viewBox="0 0 975.036 975.036"
-              >
-                <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z" />
-              </svg>
-              {/* rating */}
-              <div className="rating mb-4">
-                <input
-                  type="radio"
-                  name="rating-1"
-                  className="mask mask-star"
-                />
-                <input
-                  type="radio"
-                  name="rating-1"
-                  className="mask mask-star"
-                  defaultChecked
-                />
-                <input
-                  type="radio"
-                  name="rating-1"
-                  className="mask mask-star"
-                />
-                <input
-                  type="radio"
-                  name="rating-1"
-                  className="mask mask-star"
-                />
-                <input
-                  type="radio"
-                  name="rating-1"
-                  className="mask mask-star"
-                />
-              </div>
-              <p className="leading-relaxed mb-6">
-                &quot;I&apos;ve worked with many marketing agencies, but this
-                stands out for its personalized approach.They took the time to
-                understand our business and created a strategy tailored
-                specifically to our needs. Overall, a great experience working
-                with them.&quot;
-              </p>
-              <div className="inline-flex items-center mb-4">
-                <img
-                  alt="testimonial"
-                  src="/p3-min.webp"
-                  className="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"
-                />
-                <span className="flex-grow flex flex-col pl-4">
-                  <span className="title-font font-medium text-gray-900">
-                    Alper Kamu
-                  </span>
-                  <span className="text--gray text-sm">DESIGNER</span>
-                </span>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </section>
   );
 };
 
-export default Testimonial;
+export default Testimonial
