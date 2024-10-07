aba7be4ea89165dafde877c7dddce12d3fe11c95
Muhammad Anas <41787190+Anaskhaan@users.noreply.github.com>
2024-10-07 16:18:36 +0500

    images changed

--- src/components/main/Stats.jsx
+++ src/components/main/Stats.jsx
@@ -1,10 +1,10 @@
 const Stats = () => {
   return (
-    <div className="bg-gray-50  min-h-[350px] flex items-center justify-center font-sans">
+    <div className="bg-gray-50  min-h-[350px] flex items-center justify-center font-sans py-4">
       <div className="bg-white shadow-[0_4px_24px_-8px_rgba(0,0,0,0.2)] grid lg:grid-cols-4 sm:grid-cols-2 sm:gap-12 gap-32 rounded-3xl  p-10 mt-6">
         <div className="text-center">
           <h3 className="text-gray-800 text-4xl font-sans font-extrabold">
-            5.4<span className="text-blue-600">M+</span>
+            5.4<span className="text-orange-500">M+</span>
           </h3>
           <p className="text-gray-500 text-base font-semibold mt-3">
             Total Users
@@ -12,13 +12,13 @@ const Stats = () => {
         </div>
         <div className="text-center">
           <h3 className="text-gray-800 text-4xl font-sans font-extrabold">
-            $80<span className="text-blue-600">K</span>
+            $80<span className="text-orange-500">K</span>
           </h3>
           <p className="text-gray-500 text-base font-semibold mt-3">Revenue</p>
         </div>
         <div className="text-center">
           <h3 className="text-gray-800 text-4xl font-sans font-extrabold">
-            100<span className="text-blue-600">K</span>
+            100<span className="text-orange-500">K</span>
           </h3>
           <p className="text-gray-500 text-base font-semibold mt-3">
             Engagement
@@ -26,7 +26,7 @@ const Stats = () => {
         </div>
         <div className="text-center">
           <h3 className="text-gray-800 text-4xl font-sans font-extrabold">
-            99.9<span className="text-blue-600">%</span>
+            99.9<span className="text-orange-500">%</span>
           </h3>
           <p className="text-gray-500 text-base font-semibold mt-3">
             Server Uptime
