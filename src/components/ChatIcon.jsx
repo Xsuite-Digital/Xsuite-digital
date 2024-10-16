74c9d2af684c4c93563fe773576fa7bf80ac2edf
Mashhood-Rehman <mashhoodbutt47@gmail.com>
2024-10-16 04:22:54 -0700

    changed

--- src/components/ChatIcon.jsx
+++ src/components/ChatIcon.jsx
@@ -1,37 +1,34 @@
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
 
 const ChatIcon = () => {
-  const [visible, setVisible] = useState(false);
+    const [visible , setVisible] = useState(false)
+useEffect(()=>{
+    const togglevisibility = () => {
+        if(window.scrollY > 300) {
+            setVisible(true)
+        } else {
+            setVisible(false)
+        }
+    }
+    window.addEventListener("scroll" , togglevisibility)
+    return ()=> window.removeEventListener('scroll' , togglevisibility)
+} , [])
+    return (
+        <div
+        className={`fixed bottom-4 right-4 z-50 transition-transform duration-500 ease-in-out ${
+          visible ? "translate-x-0 opacity-100" : "translate-x-full opacity-0"
+        }`}
+      >
+        {visible && 
+        (
 
-  useEffect(() => {
-    const toggleVisibility = () => {
-      if (window.scrollY > 300) {
-        setVisible(true);
-      } else {
-        setVisible(false);
-      }
-    };
-
-    window.addEventListener("scroll", toggleVisibility);
-    return () => window.removeEventListener("scroll", toggleVisibility);
-  }, []);
-
-  return (
-    <div
-      className={`fixed bottom-4 left-4 z-50 transition-transform duration-500 ease-in-out ${
-        visible ? "translate-x-0 opacity-100" : "-translate-x-full opacity-0"
-      }`}
-    >
-      <Link to="https://wa.me/+92316361916" target="_blank">
-        <img
-          src="/whatsappicon.png"
-          alt="whatsappicon"
-          className="h-20 w-20  rounded-full hover:scale-110 transition-transform duration-300"
-        />
-      </Link>
+        <Link to='https:/wa.me/+92316361916' target="_blank">
+        <img src="/whatsappicon.png" alt="whatsappicon" height={100} width={100} className="z-50 h-16 w-16" />
+        </Link>
+        )}
     </div>
-  );
-};
+  )
+}
 
-export default ChatIcon;
+export default ChatIcon
