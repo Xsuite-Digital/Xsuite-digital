b20a04a5814948e861b375cd0fafd06d5ce24972
Mashhood-Rehman <mashhoodbutt47@gmail.com>
2024-10-30 03:46:39 -0700

    done

--- src/components/main/ExpertiseCard.jsx
+++ src/components/main/ExpertiseCard.jsx
@@ -4,7 +4,8 @@ import { Icon } from "@iconify/react/dist/iconify.js";
 /* eslint-disable-next-line react/prop-types */
 const ExpertiseCard = ({ title, icon }) => {
   return (
-    <div className="flex flex-col items-center p-6 rounded-lg transition-all duration-300 hover:shadow-lg hover:border-orange-500 hover:border-2 bg-white cursor-pointer group transform hover:scale-105">
+    <div className="flex flex-col items-center p-6 rounded-lg transition-transform duration-300 hover:shadow-lg hover:border-orange-500 hover:border-2 bg-white cursor-pointer group transform will-change-transform hover:scale-105"
+         style={{ position: 'relative', overflow: 'hidden' }}>
       <div className="p-4 rounded-full bg-gray-100 group-hover:bg-orange-100 transition-colors duration-300">
         <Icon icon={icon} className="w-8 h-8 text-gray-600 group-hover:text-orange-500" />
       </div>
