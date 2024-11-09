4861ced923db41396a04618eba64f23517ed1f6d
Mashhood-Rehman <mashhoodbutt47@gmail.com>
2024-11-09 04:16:41 -0800

    done

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import PrivacyPolicy from "./components/Footer/PrivacyPolicy";
 import ChatIcon from "./components/ChatIcon";
 import Services from "./pages/Services";
 import PackageDetails from "./pages/PackageDetails";
+import { servicesData } from "./components/main/ServicesData";
+import ServicesPage from "./components/main/ServicesPage";
 
 function App() {
   return (
@@ -21,6 +23,13 @@ function App() {
         <Route path="/Security" element={<Security />} />
         <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
         <Route path="PackageDetails" element={<PackageDetails />} />
+        {servicesData.map((service, idx) => (
+          <Route
+            key={idx}
+            path={`/services/${service.path}`}
+            element={<ServicesPage title={service.name} description={service.description} />}
+          />
+        ))}
       </Routes>
       <ChatIcon />
     </>
